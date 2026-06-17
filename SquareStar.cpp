d1f9a1e234e82f757d6b00edaf107d4e35223263
Armaghan <137501666+armaghanahmadshad@users.noreply.github.com>
2026-06-17 21:34:02 +0500

    Star pattern

--- SquareStar.cpp
+++ SquareStar.cpp
@@ -3,15 +3,14 @@ using namespace std;
 int main()
 {
     int n;
-    cout<<"Enter the number";
+    cout<<"Enter the number:";
     cin >> n;
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= n; j++)
         {
-            cout << j;
+            cout << "*";
         }
         cout << endl;
     }
-    return 0;
 }
